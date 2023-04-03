c934bc0e745c6d15c25721079cf3e48c5e0f8f1d
Gcat101 <gcatmail2@gmail.com>
2023-04-03 20:13:19 +0300

    make fabric actually usable

--- startup_scripts/depot.js
+++ startup_scripts/depot.js
@@ -1,29 +1,31 @@
-const Transaction = java('net.fabricmc.fabric.api.transfer.v1.transaction.Transaction')
-const ItemVariant = java('net.fabricmc.fabric.api.transfer.v1.item.ItemVariant')
+onEvent("loaded", e => {
+    const Transaction = java('net.fabricmc.fabric.api.transfer.v1.transaction.Transaction')
+    const ItemVariant = java('net.fabricmc.fabric.api.transfer.v1.item.ItemVariant')
 
-global.recipes = {
-    create: {
-        manual_depot_application: function (event, output, inputOnDepot, inputInHand) {
-            if (event.block == 'create:depot' && event.block.entityData.HeldItem != undefined) {
-                let depotItem = Item.of(event.block.entityData.HeldItem.Item.id, event.block.entityData.HeldItem.Item.Count, event.block.entityData.HeldItem.Item.tag)
-                let handItem = event.player.mainHandItem
-                if (inputOnDepot.equals(depotItem) && inputInHand.equals(handItem)) {
-                    event.cancel()
-                    tx = Transaction.openOuter()
-                    if (depotItem.count > (handItem.count / inputInHand.count)) {
-                        // If the remainder of ingredients is on the depot
-                        event.player.give(Item.of(output, (handItem.count / inputInHand.count), output.nbt))
-                        event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack), (handItem.count / inputInHand.count), tx)
-                    } else {
-                        // If the remainder of the ingredients is in the hand, or equal
-                        event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack),  depotItem.count, tx)
-                        event.block.entity.getItemStorage(Direction.UP).insert(ItemVariant.of(Item.of(output).itemStack), depotItem.count, tx)
+    global.recipes = {
+        create: {
+            manual_depot_application: function (event, output, inputOnDepot, inputInHand) {
+                if (event.block == 'create:depot' && event.block.entityData.HeldItem != undefined) {
+                    let depotItem = Item.of(event.block.entityData.HeldItem.Item.id, event.block.entityData.HeldItem.Item.Count, event.block.entityData.HeldItem.Item.tag)
+                    let handItem = event.player.mainHandItem
+                    if (inputOnDepot.equals(depotItem) && inputInHand.equals(handItem)) {
+                        event.cancel()
+                        tx = Transaction.openOuter()
+                        if (depotItem.count > (handItem.count / inputInHand.count)) {
+                            // If the remainder of ingredients is on the depot
+                            event.player.give(Item.of(output, (handItem.count / inputInHand.count), output.nbt))
+                            event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack), (handItem.count / inputInHand.count), tx)
+                        } else {
+                            // If the remainder of the ingredients is in the hand, or equal
+                            event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack),  depotItem.count, tx)
+                            event.block.entity.getItemStorage(Direction.UP).insert(ItemVariant.of(Item.of(output).itemStack), depotItem.count, tx)
+                        }
+                        handItem.count -= depotItem.count * inputInHand.count
+                        tx.commit()
                     }
-                    handItem.count -= depotItem.count * inputInHand.count
-                    tx.commit()
                 }
             }
         }
     }
-}
-global.recipes.createManualDepotApplication = global.recipes.create.manual_depot_application
+    global.recipes.createManualDepotApplication = global.recipes.create.manual_depot_application
+})
