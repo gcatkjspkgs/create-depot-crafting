53db7f3a0c8f6e9b7e161da6a119f4cc2bf45073
Gcat101 <gcatmail2@gmail.com>
2023-04-03 17:49:52 +0300

    forge init

--- startup_scripts/depot.js
+++ startup_scripts/depot.js
@@ -1,29 +1,30 @@
-const Transaction = java('net.fabricmc.fabric.api.transfer.v1.transaction.Transaction')
-const ItemVariant = java('net.fabricmc.fabric.api.transfer.v1.item.ItemVariant')
-
-global.recipes = {
-    create: {
-        manual_depot_application: function (event, output, inputOnDepot, inputInHand) {
-            if (event.block == 'create:depot' && event.block.entityData.HeldItem != undefined) {
-                let depotItem = Item.of(event.block.entityData.HeldItem.Item.id, event.block.entityData.HeldItem.Item.Count, event.block.entityData.HeldItem.Item.tag)
-                let handItem = event.player.mainHandItem
-                if (inputOnDepot.equals(depotItem) && inputInHand.equals(handItem)) {
-                    event.cancel()
-                    tx = Transaction.openOuter()
-                    if (depotItem.count > (handItem.count / inputInHand.count)) {
-                        // If the remainder of ingredients is on the depot
-                        event.player.give(Item.of(output, (handItem.count / inputInHand.count), output.nbt))
-                        event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack), (handItem.count / inputInHand.count), tx)
-                    } else {
-                        // If the remainder of the ingredients is in the hand, or equal
-                        event.block.entity.getItemStorage(Direction.UP).extract(ItemVariant.of(Item.of(inputOnDepot).itemStack),  depotItem.count, tx)
-                        event.block.entity.getItemStorage(Direction.UP).insert(ItemVariant.of(Item.of(output).itemStack), depotItem.count, tx)
+function depot_application (event, output, inputInHand, inputOnDepot, isAssisted, inputInOffhand, offHandHasDurability) {
+    if (event.block == 'create:depot' && event.block.entityData.HeldItem != undefined && event.player.mainHandItem.id == inputInHand) {
+        let depotItem = event.block.inventory.get(0)
+        let handItem = event.player.mainHandItem
+        let offhandItem = event.player.offHandItem
+        event.cancel()
+        if (inputOnDepot == depotItem && inputInHand == handItem && isAssisted == false) {
+            if (event.block.entityData.HeldItem.Item.id == inputOnDepot && event.player.mainHandItem == inputInHand) {
+                handItem.count--
+                event.block.inventory.extract(0, Item.of(inputOnDepot).count, false)
+                event.player.give(output)
+            }
+        } else if (inputOnDepot == depotItem && inputInHand == handItem && offhandItem == inputInOffhand && isAssisted == true) {
+            let depotItem = event.block.inventory.get(0)
+            let handItem = event.player.mainHandItem
+            let offhandItem = event.player.offHandItem
+            event.cancel()
+            if (inputOnDepot == depotItem && inputInHand == handItem && inputInOffhand == offhandItem) {
+                if (event.block.entityData.HeldItem.Item.id == inputOnDepot && event.player.mainHandItem == inputInHand && event.player.offHandItem == inputInOffhand) {
+                    handItem.count--
+                    event.block.inventory.extract(0, Item.of(inputOnDepot).count, false)
+                    event.player.give(output)
+                    if (offHandHasDurability == true) {
+                        event.player.damageHeldItem("OFF_HAND", 1)
                     }
-                    handItem.count -= depotItem.count * inputInHand.count
-                    tx.commit()
                 }
             }
         }
     }
 }
-global.recipes.createManualDepotApplication = global.recipes.create.manual_depot_application
